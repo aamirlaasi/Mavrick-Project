3c0c26f9c085599b987d69a16fc18b1169d6ee84
Jason McKinney <meter_1@yahoo.com>
2017-08-08 16:03:47 -0500

    flightinfo.js functionally complete

--- assets/java/flightinfo.js
+++ assets/java/flightinfo.js
@@ -12,60 +12,85 @@ var flightData =
 		this.airline = argAirline;
 		this.departure = argDept;
 		this.arrival = argArriv;
-		this.arrivalTime = moment.tz(argTime,argZone);
+		this.arrivalTime = moment.unix(argTime);
+		console.log(this.arrivalTime.format("dddd, MMMM Do YYYY, h:mm:ss a"));
 		if(argStatus == "En"){
 			this.status="Enroute";
 		}
 		else{
 			this.status = argStatus;
 		}
-	}
-	wirteToHTML: function(){
+	},
+	writeToHTML: function(){
 		// output to DOM
-		$('#airline').text = this.airline;
-		$('#departure').text = this.departure
-		$('#destination').text = this.arrival
-		$('#arrivalTime').text = moment.format(this.arrivalTime,"ddd"
-
+		$('#airline').text(this.airline);
+		$('#departure').text(this.departure);
+		$('#destination').text(this.arrival);
+		var timeString = this.arrivalTime.format("ddd, MMM Do , h:mm");
+		$('#arrivaltime').text(timeString);
+		$('#status').text(this.status);
 	}
 
 }
 
 function retrieveFlightStatus(flightInput){
 	//AJAX
-	var username = 'Meternx01';
-	var apiKey = 'a7bca5b55eff4c03b9e4c8adebfdf3533420973d';
-	var baseFxmlUrl = 'https://' + username + ':' + apiKey +'@flightxml.flightaware.com/json/FlightXML3/';
-	var requestOptions = {
-				'ident': $('#ident_text').val(),
+	// var username = 'Meternx01';
+	// var apiKey = 'a7bca5b55eff4c03b9e4c8adebfdf3533420973d';
+	// var baseFxmlUrl = 'https://' + username + ':' + apiKey +'@flightxml.flightaware.com/json/FlightXML3/';
+	// var requestOptions = {
+	// 			'ident': $('#ident_text').val(),
+	// 			'howMany': 10
+	// 		}
+	$.ajax({
+		url: 'https://watson-easy.herokuapp.com/flights',
+		data: {
+			args:{
+				'ident': flightInput,
 				'howMany': 10
 			}
-			$.ajax({
-				url: baseFxmlUrl + 'FlightInfoStatus',
-				data: requestOptions,
-				method: 'GET',
-				jsonp: 'jsonp_callback',
-				dataType: 'jsonp',
-				success: function(data, txtStatus, xhr) {
-					console.log(data);
-//					document.write(JSON.stringify(data));
-					if (data.error) {
-						console.log('Failed to fetch flight: ' + data.error);
-						return;
-					}
-					data.FlightInfoStatusResult.flights.forEach(flight => {
-						// Check for flight that has actually departed
-						if (flight.actual_departure_time.epoch > 0) {
-							// Display basic information about the flight
-							flightData.load(flight.airline,flight.origin.airport_name,flight.destination.airport_name,flight.estimated_arrival_time.epoch,flight.estimated_arrival_time.tz,flight.status)
-							// Display the route
-							// fetchAndRenderRoute(flight.faFlightID);
-							return ;
-						}
-					});
-				},
-				error: function(data, text) {
-					console.log('Failed to decode route: ' + data);
-				}
-			});
+		},
+		type: 'POST',
+		dataType: 'JSON'
+	}).done(function(response) {
+		console.log(response);
+		//					document.write(JSON.stringify(data));
+		if (response.error) {
+			console.log('Failed to fetch flight: ' + response.error);
+			return;
+		}
+		var i =0;
+		var found = false
+		var flightArray = response.FlightInfoStatusResult.flights;
+		// response.FlightInfoStatusResult.flights.forEach(flight => {
+		while(i < flightArray.length && !found){
+			// Check for flight that has actually departed
+			if (flightArray[i].actual_departure_time.epoch > 0) {
+				// Display basic information about the flight
+				flightData.load(flightArray[i].airline,flightArray[i].origin.airport_name,flightArray[i].destination.airport_name,flightArray[i].estimated_arrival_time.epoch,flightArray[i].estimated_arrival_time.tz,flightArray[i].status)
+				// Display the route
+				//console.log("Flight: " + JSON.stringify(flight));
+				console.log(flightData);
+				found = true ;
+			}
+			else{
+				i++;
+			}
+		};
+		flightData.writeToHTML();
+	});
 }
+$("#btn_search").on("click", function(event) {
+	// Only run this code if there is something in the input box
+	//if(searchValue!== "") {
+		// // This line grabs the input from the search box
+		// var searchValue = $("#searchInput").val().trim();
+		// // Construct a URL to search for the airport selected
+		// var queryURL = "https://avwx.rest/api/metar/" +
+		// 				searchValue + "?options=info,translate";
+		// Get weather information and push to html
+		retrieveFlightStatus($("#searchInput").val().trim());
+		// Get the iframe
+		// iFrame(weather.latitude, weather.longitude);
+	// };
+});
