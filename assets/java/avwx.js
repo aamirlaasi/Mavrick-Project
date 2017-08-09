886c3b9fb9519dd7dbf98129f34b852e6d154fa7
Aamir Laasi <aamirlaasi@Aamirs-MacBook-Air.local>
2017-08-08 20:38:41 -0500

    completed flight search resultsjs

--- assets/java/avwx.js
+++ assets/java/avwx.js
@@ -6,7 +6,7 @@
 // ------------------------------------------------
 
 // flight status output icao
-var destinationFromFlight;
+var destFlight;
 
 // Create variables for all the weather data
 var weather = {
@@ -17,6 +17,47 @@ var weather = {
 	latitude: "",
 	longitude: ""
 };
+
+// Flight information Object
+var flightData =
+{
+	// Airline Code
+	airline: "",
+	// Origin airport
+	departure: "",
+	// Desination airport
+	arrival: "",
+	// Arrival time in moment.js format
+	arrivalTime: moment(),
+	// Status E.G Enroute, Arrived, Scheduled
+	status: "",
+	// Function to load object
+	load: function(argAirline,argDept,argArriv,argTime,argZone,argStatus){
+		this.airline = argAirline;
+		this.departure = argDept;
+		this.arrival = argArriv;
+		this.arrivalTime = moment.unix(argTime);
+		// Prettys the 'En' to 'Enroute'
+		if(argStatus == "En"){
+			this.status="Enroute";
+		}
+		else{
+			this.status = argStatus;
+		}
+	},
+	// Function to write information to DOM
+	writeToHTML: function(){
+		// output to DOM
+		$('#airline').text(this.airline);
+		$('#departure').text(this.departure);
+		$('#destination').text(this.arrival);
+		// moment.js format the output
+		var timeString = this.arrivalTime.format("ddd, MMM Do , h:mm");
+		$('#arrivaltime').text(timeString);
+		$('#status').text(this.status);
+	}
+}
+
 
 // Create functions here
 // -------------------------------------------------
@@ -99,6 +140,68 @@ function iFrame(latitude,longtiude) {
 	$('#map_opensky').html(frame);	
 };
 
+// Main Function to grab Flight Status
+// requires FlightInput as string as the flight number
+function retrieveFlightStatus(flightInput){
+	//AJAX to HAM's node.js server
+	$.ajax({
+		url: 'https://watson-easy.herokuapp.com/flights',
+		data: {
+			args:{
+				'ident': flightInput,
+				'howMany': 10
+			}
+		},
+		type: 'POST',
+		dataType: 'JSON'
+	}).done(function(response) {
+		// If the flight DNE
+		if (response.error) {
+			console.log('Failed to fetch flight: ' + response.error);
+			return;
+		}
+		// set some loop variables
+		var i =0;
+		var found = false
+		// simplify the response
+		var flightArray = response.FlightInfoStatusResult.flights;
+		// Go looping for a departed flight
+		while(i < flightArray.length && !found){
+			// Check for flight that has actually departed
+			if (flightArray[i].actual_departure_time.epoch > 0) {
+				// Load that flight data to the flight object
+				flightData.load(flightArray[i].airline,flightArray[i].origin.airport_name,flightArray[i].destination.airport_name,flightArray[i].estimated_arrival_time.epoch,flightArray[i].estimated_arrival_time.tz,flightArray[i].status)
+				// set to Found so loop exits.
+				found = true ;
+			}
+			else{	// else iterate
+				i++;
+			}
+		};
+		// Write to the DOM
+		flightData.writeToHTML();
+		destFlight = response.FlightInfoStatusResult.flights[i].destination.code;
+		// Construct a URL to search for the airport selected 
+		var queryURL = "https://avwx.rest/api/metar/" +
+					 destFlight + "?options=info,translate";
+		// Get weather information and push to html
+		avwx(queryURL);
+		// Update the longitude an latitude of airport
+		search(destFlight, airportsArray);
+		// Get the iframe
+		iFrame(weather.latitude, weather.longitude);
+	});
+};
+
+// Function to search for ICAO in airports array
+function search(nameKey, myArray){
+    for (var i=0; i < myArray.length; i++) {
+        if (myArray[i].icao === nameKey) {
+			weather.longitude = myArray[i].lon;
+            weather.latitude = myArray[i].lat;
+        }
+    }
+}
 
 // Execute the main code here
 // -------------------------------------------------
@@ -151,9 +254,7 @@ $("#searchInput").on("keypress", function(event) {
 		var searchValue = $("#searchInput").val().trim();
 		// Update the flight status and store destination icao
 		retrieveFlightStatus(searchValue);
-		console.log(typeof(destinationFromFlight));
 	};
-
 });
 
 
